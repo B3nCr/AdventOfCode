a87eead99363a57cb87ef35a55402c048703d060
Ben Crinion <ben.crinion@sorted.com>
2020-12-03 13:08:14 +0000

    Day 3

--- 3/index.js
+++ 3/index.js
@@ -1,3 +1,32 @@
 import fs from "fs";
 
-var array = fs.readFileSync("./2/file.txt").toString().split("\r\n");
+var array = fs.readFileSync("./3/file.txt").toString().split("\r\n");
+
+const length = array[0].length;
+
+var count = CountTrees(1, 1) *
+  CountTrees(3, 1) *
+  CountTrees(5, 1) *
+  CountTrees(7, 1) *
+  CountTrees(1, 2);
+
+console.log(count);
+
+function CountTrees(right, down) {
+  var x = 0;
+  var count = 0;
+
+  for (var y = 0; y < array.length; y += down) {
+    var element = array[y];
+    if (element[x] === "#") {
+      count++;
+    }
+    console.log(`${element[x]} ${x},${y} ${count}`);
+
+    x += right;
+    if (x >= length) {
+      x = x - length;
+    }
+  }
+  return count;
+}
