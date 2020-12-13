7fcbd194b2afefae94d73b643f4731b66405ae5d
Ben Crinion <ben.crinion@sorted.com>
2020-12-13 20:17:38 +0000

    Day 9

--- 9/main_test.go
+++ 9/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestValidate(t *testing.T) {
 	testData := []int{
@@ -24,8 +26,38 @@ func TestValidate(t *testing.T) {
 		277,
 		309,
 		576}
-	result := Validate(testData[0:15], 5)
-	if !result {
+	_, result := Validate(testData, 5)
+	if result != 127 {
+		t.Error("test failure")
+	}
+}
+
+func TestFindSequence(t *testing.T) {
+	testData := []int{
+		35,
+		20,
+		15,
+		25,
+		47,
+		40,
+		62,
+		55,
+		65,
+		95,
+		102,
+		117,
+		150,
+		182,
+		127,
+		219,
+		299,
+		277,
+		309,
+		576}
+
+	low, high := FindSequence(testData, 14, 127)
+
+	if low == 0 || high == 0 {
 		t.Error("test failure")
 	}
 }
