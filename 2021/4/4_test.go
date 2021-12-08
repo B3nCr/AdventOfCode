8fb7f07e5c1a51ac76b0955be1e7a607128742e9
Ben Crinion <77635121+BenCrinion-IW@users.noreply.github.com>
2021-12-08 23:59:12 +0000

    Reading board

--- 2021/4/4_test.go
+++ 2021/4/4_test.go
@@ -32,6 +32,70 @@ func TestReadInput(t *testing.T) {
 	fmt.Print(boards)
 }
 
+func (b Board) GetResult(draw []int) Board {
+	for round, pick := range draw {
+		found := false
+		for row := range b.Layout { // replace with range
+			for col := range b.Layout[row] {
+				if b.Layout[row][col] == pick {
+					b.State[row][col] = true
+					found = true
+					break
+				}
+			}
+			if found {
+				break
+			}
+		}
+		b.Round = round + 1 // because of 0 index
+		if b.State.won() {
+			b.HasWon = true
+			b.Score = b.CalcScore(pick)
+			return b
+		}
+	}
+
+	return b
+}
+
+func (b Board) CalcScore(pick int) int {
+	score := 0
+	for row := range b.Layout {
+		for col := range b.Layout[row] {
+			if !b.State[row][col] {
+				score += b.Layout[row][col]
+			}
+		}
+	}
+	score *= pick
+	return score
+}
+
+func (s BoardState) won() bool {
+	result := false
+	for _, row := range s {
+		result = checkLine(row)
+		if result {
+			return result
+		}
+	}
+	for colNum := range s {
+		var column [5]bool
+		for i := 0; i < 5; i++ {
+			column[i] = s[i][colNum]
+		}
+		result = checkLine(column)
+		if result {
+			return result
+		}
+	}
+	return result
+}
+
+func checkLine(line [5]bool) bool {
+	return line[0] && line[1] && line[2] && line[3] && line[4]
+}
+
 func ReadInput(r io.Reader) ([]int, []BoardLayout) {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanLines)
