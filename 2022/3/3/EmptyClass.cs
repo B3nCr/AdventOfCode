04ac1dd6542aa16a918d72ba35997a5833a50481
Ben Crinion <77635121+BenCrinion-IW@users.noreply.github.com>
2022-12-03 23:51:48 +0000

    2022-3 Benchmark

---
+++ 2022/3/3/EmptyClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace _3
+{
+	public class EmptyClass
+	{
+        [Benchmark]
+		public int GetPriproty()
+		{
+            var lines = File.ReadAllLines("input.txt");
+
+            var total = 0;
+            for (int i = 0; i < lines.Length; i += 3)
+            {
+                var part1 = lines[i];
+                var part2 = lines[i + 1];
+                var part3 = lines[i + 2];
+
+                var union = part1.Intersect(part2).Intersect(part3);
+
+                total += GetPriority(union.First());
+
+            }
+            return total;
+        }
+
+        private static int GetPriority(char itemType)
+        {
+            if (char.IsUpper(itemType))
+            {
+                return (int)itemType - 65 + 27;
+            }
+
+            return itemType - 96;
+        }
+    }
+}
+
--- 2022/3/3/Program.cs
+++ 2022/3/3/Program.cs
@@ -1,39 +1,10 @@
-﻿namespace _3;
+﻿using BenchmarkDotNet.Running;
+
+namespace _3;
 class Program
 {
     static void Main(string[] args)
     {
-        var lines = File.ReadAllLines("input.txt");
-
-        var total = 0;
-        for (int i = 0; i < lines.Length; i+=3)
-        {
-
-            //var mid = lines[i].Length / 2;
-            var part1 = lines[i];
-            var part2 = lines[i+1];
-            var part3 = lines[i + 2];
-
-            var union = part1.Intersect(part2).Intersect(part3);
-
-            Console.WriteLine(union.First());
-
-            total += GetPriority(union.First());
-
-            //Console.WriteLine(union.Count());
-            //Console.WriteLine(part1);
-
-        }
-        Console.WriteLine(total);
-    }
-
-    private static int GetPriority(char itemType)
-    {
-        if (char.IsUpper(itemType))
-        {
-            return (int)itemType - 65 + 27;
-        }
-
-        return itemType - 96;
+        var summary = BenchmarkRunner.Run<EmptyClass>();
     }
 }
