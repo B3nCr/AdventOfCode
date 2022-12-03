6110c3dcdac65f2e10273fa29793da932d7c2df7
Ben Crinion <77635121+BenCrinion-IW@users.noreply.github.com>
2022-12-03 23:24:10 +0000

    Day 3.2

--- 2022/3/3/Program.cs
+++ 2022/3/3/Program.cs
@@ -6,20 +6,23 @@ class Program
         var lines = File.ReadAllLines("input.txt");
 
         var total = 0;
-        for (int i = 0; i < lines.Length; i++)
+        for (int i = 0; i < lines.Length; i+=3)
         {
-            var mid = lines[i].Length / 2;
-            var part1 = lines[i].Substring(0, mid);
-            var part2 = lines[i].Substring(mid, lines[i].Length - mid);
 
-            var union = part1.Intersect(part2);
+            //var mid = lines[i].Length / 2;
+            var part1 = lines[i];
+            var part2 = lines[i+1];
+            var part3 = lines[i + 2];
 
+            var union = part1.Intersect(part2).Intersect(part3);
+
+            Console.WriteLine(union.First());
 
             total += GetPriority(union.First());
 
             //Console.WriteLine(union.Count());
             //Console.WriteLine(part1);
-            
+
         }
         Console.WriteLine(total);
     }
