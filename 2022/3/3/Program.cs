1463a0321483c37dcb3d1c93478eec3aa895c9da
Ben Crinion <77635121+BenCrinion-IW@users.noreply.github.com>
2022-12-03 23:15:02 +0000

    Day 3.1

---
+++ 2022/3/3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines("input.txt");
+
+        var total = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var mid = lines[i].Length / 2;
+            var part1 = lines[i].Substring(0, mid);
+            var part2 = lines[i].Substring(mid, lines[i].Length - mid);
+
+            var union = part1.Intersect(part2);
+
+
+            total += GetPriority(union.First());
+
+            //Console.WriteLine(union.Count());
+            //Console.WriteLine(part1);
+            
+        }
+        Console.WriteLine(total);
+    }
+
+    private static int GetPriority(char itemType)
+    {
+        if (char.IsUpper(itemType))
+        {
+            return (int)itemType - 65 + 27;
+        }
+
+        return itemType - 96;
+    }
+}
